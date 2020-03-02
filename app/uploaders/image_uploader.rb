eefb21342f07e361cba46d800541eae0ed64459d
ryo-hanegawa <snake0515hane@gmail.com>
2020-03-02 20:31:41 +0900

    commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,10 +3,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
+
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
   process resize_to_fit: [800, 800]
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
